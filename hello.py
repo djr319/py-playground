fc562b89e6762c5e05f4a9d7ef11c93f166943e5
David Ryan <mail@djryan.eu>
2022-05-11 11:19:49 +0200

    feat: import numpy

--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
-variable = 'world'
-print('Hello ' + variable)
+# Start virtual environment: venv\Scripts\activate
+
+# pip install numpy
+import numpy as np
+
+variable = 'World'
+variable = input("Enter your name: ")
+print('Hello\n' + variable)
+
+
+def my_function():
+  print('variable is a ', type(variable))
+  f"variable is a {type(variable)}"
+my_function()
